572ff0241292a4ae246ad131cbad6663db6ac20a
Alex Petrov <kocetomad@pop-os.localdomain>
2022-11-19 18:45:28 +0000

    cms modal

--- game-store/src/components/pages/CMS_Layouts/InventoryList.js
+++ game-store/src/components/pages/CMS_Layouts/InventoryList.js
@@ -3,6 +3,25 @@ import { FaTrash,FaEdit,FaPlusSquare,FaKey } from 'react-icons/fa';
 const InventoryList = () => {
   return (
     <div className="overflow-x-auto mx-8">
+      <label for="my-modal-6" class="flex-1 btn btn-success">open modal</label>
+      <input type="checkbox" id="my-modal-6" class="modal-toggle" />
+      <div class="modal modal-bottom sm:modal-middle">
+        <div class="modal-box">
+          <h3 class="font-bold text-lg">
+            Congratulations random Internet user!
+          </h3>
+          <p class="py-4">
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+          <div class="modal-action">
+            <label for="my-modal-6" class="btn">
+              Yay!
+            </label>
+          </div>
+        </div>
+      </div>
+      
         <div className="btn-group btn-group-vertical lg:btn-group-horizontal flex ">
   <button className="flex-1 btn btn-success">Add new listing <FaPlusSquare/></button>
   <button className="flex-1 btn btn-secondary">Add new standalone key <FaKey/></button>
